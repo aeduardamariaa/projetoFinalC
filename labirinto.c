920f15a5e2dfe306aebaa1603d8000316f1bf336
Jessica Fedalto <fedaltojessica@gmail.com>
2023-11-07 10:20:09 -0300

    Ganhando o jogo

--- labirinto.c
+++ labirinto.c
@@ -47,7 +47,13 @@ void mostrarLabirinto(int matriz[LINHA][COLUNA], Personagem bolinha)
             else if (matriz[i][j] == 0)
             {
                 printf("  ");
-            }else if(matriz[i][j] == 2){
+            }
+            else if (matriz[i][j] == 2)
+            {
+                printf("  ");
+            }
+            else if (matriz[i][j] == 3)
+            {
                 printf("  ");
             }
         }
@@ -55,54 +61,110 @@ void mostrarLabirinto(int matriz[LINHA][COLUNA], Personagem bolinha)
     }
 }
 
-
-// void verificarParedes(int matriz[LINHA][COLUNA], Personagem bolinha, char input)
-// {
-//     for (int i = 0; i < LINHA; i++)
-//     {
-//         for (int i = 0; i < COLUNA; i++)
-//         {
-//             if ()
-//             {
-//                 /* code */
-//             }
-
-//         }
-
-//     }
-//     return 1;
-// }
-void andar(int matriz[LINHA][COLUNA], Personagem * bolinha)
+int verificarParedes(int matriz[LINHA][COLUNA], Personagem bolinha, int input)
 {
-    while(1){
+
+    if (input == 87 || input == 119) //W
+    {
+        if (matriz[bolinha.x -1][bolinha.y] == 1)
+        {
+            return 1;
+        }
+        else if (matriz[bolinha.x -1][bolinha.y] == 3)
+        {
+            return 3;
+        }
+    }
+    else if (input == 68 || input == 100) // D
+    {
+        if (matriz[bolinha.x][bolinha.y + 1] == 1)
+        {
+            return 1;
+        }
+        else if (matriz[bolinha.x ][bolinha.y+1] == 3)
+        {
+            return 3;
+        }
+    }
+    else if (input == 83 || input == 115) // S
+    {
+        if (matriz[bolinha.x + 1][bolinha.y] == 1)
+        {
+            return 1;
+        }
+        else if (matriz[bolinha.x +1][bolinha.y] == 3)
+        {
+            return 3;
+        }
+    }
+    else if (input == 65 || input == 97) // A
+    {
+        if (matriz[bolinha.x][bolinha.y - 1] == 1)
+        {
+            return 1;
+        }
+        else if (matriz[bolinha.x ][bolinha.y -1] == 3)
+        {
+            return 3;
+        }
+    }
+    else{
+        return 0;
+    }
+  
+}
+
+int andar(int matriz[LINHA][COLUNA], Personagem *bolinha)
+{
+    while (1)
+    {
         mostrarLabirinto(matriz, *bolinha);
 
-        int input; 
+        int input;
 
         input = getch();
 
         if (kbhit() == 0)
         {
             // W
-            if (input == 87 || input == 119)
+            if (input == 87 || input == 119 && verificarParedes(matriz, *bolinha, input) == 0)
             {
                 bolinha->x = bolinha->x - 1;
             }
-            else if (input == 68 || input == 100) //D
+            else if (input == 68 || input == 100 && verificarParedes(matriz, *bolinha, input) == 0) // D
             {
                 bolinha->y = bolinha->y + 1;
             }
-            else if (input == 83 || input == 115)//S
+            else if (input == 83 || input == 115 && verificarParedes(matriz, *bolinha, input) == 0) // S
             {
                 bolinha->x = bolinha->x + 1;
             }
-            else if (input == 65 || input == 97) //A
+            else if (input == 65 || input == 97 && verificarParedes(matriz, *bolinha, input) == 0) // A
             {
                 bolinha->y = bolinha->y - 1;
             }
-            system("cls");  
+            else if(verificarParedes(matriz, *bolinha, input) == 3){
+                printf("Ganhou!");
+                break;
+            }
+            else //if (verificarParedes(matriz, *bolinha, input) == 1)
+            {
+                bolinha->x = 13;
+                bolinha->y = 0;
+            }
+            system("cls");
         }
     }
+}
+
+
+int ganhar(int matrriz[LINHA][COLUNA], Personagem bolinha)
+{
+    if (bolinha.x == 3 && bolinha.y == 16)
+    {
+        return 3;
+    }
+    
 }
 
 int main(void)
@@ -111,11 +173,6 @@ int main(void)
     bolinha.x = 13;
     bolinha.y = 0;
 
-    while (1)
-    {
-        // mostrarLabirinto(matriz, bolinha);
-        andar(matriz, &bolinha);
-        // printf("%d", inputUsuario());
-        break;
-    }
+    andar(matriz, &bolinha);
+
 }
