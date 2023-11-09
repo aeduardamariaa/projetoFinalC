227a05c7d5e77b4f228c5a2c82e4456c85827eb2
Yasmin Trembulack Agostinho <142899321+YasminTrembulack@users.noreply.github.com>
2023-11-09 16:41:06 -0300

    Update Textos.c

--- Textos.c
+++ Textos.c
@@ -19,30 +19,62 @@ int main(){
 }
 
 
+	// printf("    %c%c      %c%c%c%c    %c%c%c%c%c%c    %c%c    %c%c%c%c                          
+	// printf("    %c%c%c%c    %c%c%c%c%c%c%c%c  %c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c                         
+	// printf("    %c%c%c%c  %c%c%c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c                    
+	// printf("      %c%c%c%c%c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c                
+	// printf("        %c%c%c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c
+	// printf("        %c%c%c%c%c%c    %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c                       
+	// printf("        %c%c%c%c%c%c      %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c                             
+	// printf("          %c%c%c%c        %c%c%c%c%c%c      %c%c%c%c%c%c                             
 
-// printf("    %c%c      %c%c%c%c    %c%c%c%c%c%c    %c%c    %c%c%c%c                          
-// printf("    %c%c%c%c    %c%c%c%c%c%c%c%c  %c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c                         
-// printf("    %c%c%c%c  %c%c%c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c                    
-// printf("      %c%c%c%c%c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c                
-// printf("        %c%c%c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c
-// printf("        %c%c%c%c%c%c    %c%c%c%C  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c                       
-// printf("        %c%c%c%c%c%c      %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c                             
-// printf("          %c%c%c%c        %c%c%c%c%c%c      %c%c%c%c%c%c                             
 
 
-// \n",fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,	fontA,fontA,fontA,fontA,fontA,fontA );
 
-// \n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA	    ,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+// printf("    %i%i      %i%i%i%i    %i%i%i%i%i%i    %i%i    %i%i%i%i \n",fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,	fontA,fontA,fontA,fontA,fontA,fontA );                          
+// printf("    %i%i%i%i    %i%i%i%i%i%i%i%i  %i%i%i%i%i%i  %i%i%i%i  %i%i%i%i%i%i\n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA	    ,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+// printf("    %i%i%i%i  %i%i%i%i%i%i%i%i%i%i%i%i  %i%i%i%i%i%i%i%i%i%i  %i%i%i%i%i%i\n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB	,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);                    
+// printf("      %i%i%i%i%i%i%i%i%i%i  %i%i%i%i  %i%i%i%i%i%i%i%i%i%i  %i%i%i%i%i%i\n",fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB		,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);                
+// printf("        %i%i%i%i%i%i%i%i  %i%i%i%i  %i%i%i%i%i%i%i%i%i%i  %i%i%i%i%i%i\n",fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+// printf("        %i%i%i%i%i%i    %i%i%i%i  %i%i%i%i%i%i%i%i%i%i  %i%i%i%i%i%i\n",fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB					,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+// printf("        %i%i%i%i%i%i      %i%i%i%i%i%i%i%i%i%i  %i%i%i%i%i%i%i%i%i%i\n",fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB);
+// printf("          %i%i%i%i        %i%i%i%i%i%i      %i%i%i%i%i%i\n",fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB);                             
 
-// \n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB	,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
 
-// \n",fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB		,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB	);
+// printf("    %c%c      %c%c%c%c    %c%c%c%c%i%c    %c%c    %c%c%c%c \n",fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,	fontA,fontA,fontA,fontA,fontA,fontA );                          
+// printf("    %c%c%c%c    %c%c%c%c%c%c%c%c  %c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c\n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA	    ,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+// printf("    %c%c%c%c  %c%c%c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c\n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB	,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);                    
+// printf("      %c%c%c%c%c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c\n",fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB		,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);                
+// printf("        %c%c%c%c%c%c%c%c  %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c\n",fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+// printf("        %c%c%c%c%c%c    %c%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c\n",fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB					,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+// printf("        %c%c%c%c%c%c      %c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c\n",fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB);
+// printf("          %c%c%c%c        %c%c%c%c%c%c      %c%c%c%c%c%c\n",fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB);                             
 
-// \n",fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
 
-// \n",fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB					,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
 
-// \n",fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB);
 
-// \n",fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,,fontB,fontB,fontB,fontB,fontB,fontB);
+// printf("    %c%c      %c%c%c%c  %c%c%c%c  %c%c      %c%c%c%c  %c%c%c%c                  
+// printf("    %c%c%c%c    %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c    %c%c%c%c%c%c%c%c%c%c%c%c                  
+// printf("    %c%c%c%c    %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c%c%c               
+// printf("    %c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c  %c%c%c%c%c%c%c%c%c%c%c%c%c%c
+// printf("    %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c    %c%c%c%c%c%c%c%c%c%c%c%c                
+// printf("    %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c    %c%c%c%c%c%c  %c%c%c%c                               
+// printf("      %c%c  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c    %c%c%c%c%c%c%c%c%c%c  
+// printf("        %c%c  %c%c%c%c    %c%c%c%c  %c%c      %c%c%c%c  %c%c%c%c                     
 
+
+// \n",fontA,fontA,fontA,fontA,,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA);
+
+// \n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+
+// \n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+
+// \n",fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontA,fontA,fontB,fontB	,fontA,fontA,fontA,fontA,fontB,fontB);
+
+// \n",fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB);
+
+// \n",fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontB,fontB,fontB,fontB);
+
+// \n",fontA,fontA,,fontA,fontA,,fontA,fontA,fontB,fontB,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA,fontB,fontB,fontA,fontA,fontA,fontA);
+
+// \n",fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB,fontB);
