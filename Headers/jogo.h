b084a45e5da7e46b6d86f188523e4f8053aa6d4a
Jessica Fedalto <fedaltojessica@gmail.com>
2023-11-10 10:55:26 -0300

    Alteração da header

--- Headers/jogo.h
+++ Headers/jogo.h
@@ -11,10 +11,13 @@ typedef struct
 int LINHA;
 int COLUNA;
 
+void ganhou();
 void mostrarLabirinto(int matriz[LINHA][COLUNA], Personagem bolinha);
 int verificarParedes(int matriz[LINHA][COLUNA], Personagem bolinha, int input);
 int ganhar(int matriz[LINHA][COLUNA], Personagem bolinha);
 void andar(int matriz[LINHA][COLUNA], Personagem *bolinha, int input);
+struct tm tempoAtual();
+float seconds(struct tm *data_hora_atual);
 void jogo(int matriz[LINHA][COLUNA], Personagem bolinha);
 
 #endif
