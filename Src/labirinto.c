f5d3fa062c6b39d5afdc62eb4e158293bb470f84
Jessica <fedaltojessica@gmail.com>
2023-11-15 14:58:23 -0300

    Arrumando bugs

--- Src/labirinto.c
+++ Src/labirinto.c
@@ -36,10 +36,26 @@ void menuDificuldades()
     printf("\n\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",c,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,d);
 }
 
+int verificarOpcao(int op, int array[10], int tamanho)
+{
+    for (int i = 0; i < tamanho; i++)
+        {
+            if (op == array[i])
+            {
+                return 1;
+            }
+        }
+}
+
 int main(void)
 {
     int op = 10;
+    int escolha = 10;
     Personagem bolinha;
+    int array1[10] = {0,1,2,3,4,5,6,7,8,9};
+    int array2[3] = {1,2,3};
+    int verdade = 0;
+    int tamanho = 10;
 
     while (op != 0)
     {
@@ -47,58 +63,82 @@ int main(void)
         menu();
         printf("\n\tQual sua escolha: ");
         scanf("%d", &op);
-        switch (op)
+        
+        verdade = verificarOpcao(op, array1, tamanho);
+        if (verdade == 1)
         {
-        case 1:
-            system("cls");
-            menuDificuldades();
-            printf("\n\tQual sua escolha: ");
-            scanf("%d", &op);
-            system("cls");
+            verdade = 0;
+            tamanho = 3;
             switch (op)
             {
                 case 1:
-                    bolinha.x = 13;
-                    bolinha.y = 0;
-
-                    LINHA =17;
-                    COLUNA=17;
-
-                    jogo(matriz1, bolinha);
-
-                    break;
+                system("cls");
+                menuDificuldades();
+                printf("\n\tQual sua escolha: ");
+                scanf("%d", &escolha);
                 
-                case 2:
-                    bolinha.x = 0;
-                    bolinha.y = 1;
+                verdade = verificarOpcao(escolha, array2, tamanho); 
 
-                    LINHA =35;
-                    COLUNA=35;
+                if (verdade == 1)
+                {
+                    system("cls");
+                    switch (escolha)
+                    {
+                        case 1:
+                            bolinha.x = 13;
+                            bolinha.y = 0;
 
-                    jogo(matriz2, bolinha);
+                            LINHA =17;
+                            COLUNA=17;
+
+                            jogo(matriz1, bolinha);
+
+                            break;
+                        
+                        case 2:
+                            bolinha.x = 0;
+                            bolinha.y = 1;
+
+                            LINHA =35;
+                            COLUNA=35;
+
+                            jogo(matriz2, bolinha);
+                            break;
+                        case 3:
+
+                            bolinha.x = 0;
+                            bolinha.y = 1;
+
+                            LINHA =50;
+                            COLUNA=50;
+
+                            jogo(matriz3, bolinha);
+                            break;
+                        case 4:
+                            break;
+                        // default:
+                        //     printf("\nSaindo!");
+                        //     break;
+                    }
+                }
+                else
+                {
+                    system("cls");
+                    printf("\nOperacao invalida, tente novamente!!");
                     break;
-                case 3:
-
-                    bolinha.x = 0;
-                    bolinha.y = 1;
-
-                    LINHA =50;
-                    COLUNA=50;
-
-                    jogo(matriz3, bolinha);
-                    break;
-
-                case 4:
-                    break;
-                default:
-                    printf("\nSaindo!");
+                }
+            case 2:
+                printf("rankng!");
+                break;
+            default:
+                printf("saundo");
+                break;
             }
-            break;
-        case 2:
-            printf("rankng!");
-            break;
-        default:
-            printf("saundo");
+        }
+        else
+        {
+            system("cls");
+            printf("\nOperacao invalida, tente novamente!!");
             break;
         }
     }
